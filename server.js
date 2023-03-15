9f4778dd82d1fe86e3fe65b9e97a1a69b54678ea
José Cordeiro <zecaxcr@gmail.com>
2023-03-15 20:23:40 -0300

    update

--- server.js
+++ server.js
@@ -12,18 +12,21 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With,content-type, Authorization');
+    res.setHeader("Access-Control-Expose-Headers", "Authorization");
     next();
+
+app.use(bodyParser.json())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
  });
 
 app.use('/api', api)
